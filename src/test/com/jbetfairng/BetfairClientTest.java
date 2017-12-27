0194ca92ce59ddfba86260bf76c8b2d7fef96900
catalin.mincu <cata_mincu@yahoo.com>
2017-12-27 13:30:25 +0200

    Adding Spring. Externalize configuration.

--- src/test/com/jbetfairng/BetfairClientTest.java
+++ src/test/com/jbetfairng/BetfairClientTest.java
@@ -1,26 +1,37 @@
 package com.jbetfairng;
 
+import com.jbetfairng.config.AppConfig;
 import com.jbetfairng.entities.*;
 import com.jbetfairng.enums.*;
 import com.jbetfairng.exceptions.LoginException;
 import com.jbetfairng.util.Helpers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 /**
  * Created by catalin on 4/19/2017.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
 public class BetfairClientTest {
-
+    @Autowired
+    AppConfig appConfig;
 
     @Test
     public void listMarketCatalogue() throws Exception {
 
-        BetfairClient client = new BetfairClient(Exchange.RO, "app_key");
+        BetfairClient client = new BetfairClient(Exchange.RO, appConfig.getAppKey());
         try {
-            client.login("certificate_location", "certificate_password", "username", "parola");
+            client.login(appConfig.getCertificateFile(), appConfig.getCertificatePassword(), appConfig.getUserName(), appConfig.getPassword());
         } catch (LoginException exc) {
             System.out.println(exc);
         }
@@ -37,23 +48,21 @@ public class BetfairClientTest {
                 MarketSort.FIRST_TO_START, 40).getResponse();
 
         for (MarketCatalogue marketCatalogue : marketCatalogueList) {
+            System.out.println("Market Name: " + marketCatalogue.getMarketName() + "; Id: " + marketCatalogue.getMarketId()
+                    + ";Competition: " + marketCatalogue.getCompetition() + "; Time: " + marketCatalogue.getDescription().getMarketTime() + "\n");
+
+            List<RunnerCatalog> runners = marketCatalogue.getRunners();
+            if (runners != null) {
+                for (RunnerCatalog rCat : runners) {
+                    System.out.println("Runner Name: " + rCat.getRunnerName() + "; Selection Id: " + rCat.getSelectionId() + "\n");
+                }
+            }
+
+            // this call is only to see which are available bets for an event
             List<MarketBook> marketBookList = client.listMarketBook(Collections.singletonList(marketCatalogue.getMarketId()), Helpers.soccerPriceProjection(),
                     null, null).getResponse();
-
-            printMarketCatalogue(marketCatalogue);
-        }
-
-    }
-
-
-    private void printMarketCatalogue(MarketCatalogue mk) {
-        System.out.println("Market Name: " + mk.getMarketName() + "; Id: " + mk.getMarketId()
-                + ";Competition: " + mk.getCompetition() + "; Time: " + mk.getDescription().getMarketTime() + "\n");
-
-        List<RunnerCatalog> runners = mk.getRunners();
-        if (runners != null) {
-            for (RunnerCatalog rCat : runners) {
-                System.out.println("Runner Name: " + rCat.getRunnerName() + "; Selection Id: " + rCat.getSelectionId() + "\n");
+            for (MarketBook marketBook : marketBookList) {
+                System.out.println(marketBook.getRunners());
             }
         }
     }
